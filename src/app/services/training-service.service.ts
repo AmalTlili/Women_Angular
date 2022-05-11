a8eb2ebb79a41c080d7fa661dae7b165566b661f
ahmed khiari <47713853+Kaysharp42@users.noreply.github.com>
2022-05-12 00:10:20 +0100

    update

--- src/app/services/training-service.service.ts
+++ src/app/services/training-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpHeaders, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Training} from '../models/Training.model';
 const baseUrl = 'http://localhost:8081/women/Training';
@@ -7,31 +7,33 @@ const baseUrl = 'http://localhost:8081/women/Training';
   providedIn: 'root'
 })
 export class TrainingServiceService {
-  constructor(private httpClient: HttpClient) { }
+    httpOptions = { headers: new HttpHeaders().set('Authorization',`Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJVc2VyIE1hbmFnZW1lbnQgUG9ydGFsIiwic3ViIjoiZmFyd2ExIiwiaXNzIjoiR2V0IEFycmF5cywgTExDIiwiZXhwIjoxNjUyNDMyNDE5LCJpYXQiOjE2NTIwMDA0MTksImF1dGhvcml0aWVzIjpbInVzZXI6cmVhZCIsInVzZXI6Y3JlYXRlIiwidXNlcjp1cGRhdGUiLCJ1c2VyOmRlbGV0ZSJdfQ.1cBZ-El79IjkoUTSbye8idcd1e4Rb_F6fg_PzBb84eQ8JAwvkbXdLRaNT3UfrV3VkhVdpRaBxUZ8bwY3i8yc1A"}`)};
+
+    constructor(private httpClient: HttpClient) { }
 
     getTrainings(): Observable<Training[]> {
-        return this.httpClient.get<Training[]>(`${baseUrl}/getTrainings`);
+        return this.httpClient.get<Training[]>(`${baseUrl}/getTrainings`, this.httpOptions);
   }
     get(id: any): Observable<Training> {
         // @ts-ignore
-        return this.httpClient.get(`${baseUrl}/getTraining/${id}`);
+        return this.httpClient.get(`${baseUrl}/getTraining/${id}`, this.httpOptions);
     }
 
     create(data: any): Observable<any> {
-        return this.httpClient.post(`${baseUrl}/add`, data);
+        return this.httpClient.post(`${baseUrl}/add`, data, this.httpOptions);
     }
 
     update(id: any, data: any): Observable<any> {
-        return this.httpClient.put(`${baseUrl}/update/${id}`, data);
+        return this.httpClient.put(`${baseUrl}/update/${id}`, data, this.httpOptions);
     }
     assignFormerToTrainer(userId: any, trainingId: any): Observable<any> {
-        return this.httpClient.post(`${baseUrl}/assignatrainer/${userId}/${trainingId}`, null);
+        return this.httpClient.post(`${baseUrl}/assignatrainer/${userId}/${trainingId}`, null, this.httpOptions);
     }
     assignUserToTrainer(userId: any, trainingId: any): Observable<any> {
-        return this.httpClient.post(`${baseUrl}/assignauser/${userId}/${trainingId}`, null);
+        return this.httpClient.post(`${baseUrl}/assignauser/${userId}/${trainingId}`, null, this.httpOptions);
     }
     delete(id: any): Observable<any> {
-        return this.httpClient.delete(`${baseUrl}/remove/${id}`);
+        return this.httpClient.delete(`${baseUrl}/remove/${id}`, this.httpOptions);
     }
 
     deleteAll(): Observable<any> {
@@ -39,9 +41,9 @@ export class TrainingServiceService {
     }
 
     findByTitle(title: any): Observable<Training[]> {
-        return this.httpClient.get<Training[]>(`${baseUrl}?title=${title}`);
+        return this.httpClient.get<Training[]>(`${baseUrl}?title=${title}`, this.httpOptions);
     }
     search(domain: any, startDate: any, endDate: any): Observable<Training[]> {
-        return this.httpClient.get<Training[]>(`${baseUrl}/search/${domain}/${startDate}/${endDate}`);
+        return this.httpClient.get<Training[]>(`${baseUrl}/search/${domain}/${startDate}/${endDate}`, this.httpOptions);
     }
 }
--- src/app/services/upload-files.service.ts
+++ src/app/services/upload-files.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpHeaders, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
@@ -7,20 +7,25 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class UploadFileService {
+  httpOptions = { headers: new HttpHeaders().set('Authorization',`Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJVc2VyIE1hbmFnZW1lbnQgUG9ydGFsIiwic3ViIjoiZmFyd2ExIiwiaXNzIjoiR2V0IEFycmF5cywgTExDIiwiZXhwIjoxNjUyNDMyNDE5LCJpYXQiOjE2NTIwMDA0MTksImF1dGhvcml0aWVzIjpbInVzZXI6cmVhZCIsInVzZXI6Y3JlYXRlIiwidXNlcjp1cGRhdGUiLCJ1c2VyOmRlbGV0ZSJdfQ.1cBZ-El79IjkoUTSbye8idcd1e4Rb_F6fg_PzBb84eQ8JAwvkbXdLRaNT3UfrV3VkhVdpRaBxUZ8bwY3i8yc1A"}`)};
 
   constructor(private http: HttpClient) {
   }
 
-  upload(file: File, id: number): Observable<HttpEvent<any>> {
+  upload(file: File, id: number): Observable<any> {
     const formData: FormData = new FormData();
 
     formData.append('file', file);
 
+    // @ts-ignore
     const req = new HttpRequest('POST', `http://localhost:8081/women/Training/upload/${id}`, formData, {
       reportProgress: true,
       responseType: 'json'
     });
-
-    return this.http.request(req);
+    return this.http.post(`http://localhost:8081/women/Training/upload/${id}`, formData, {
+      reportProgress: true,
+      responseType: 'json',
+      headers: new HttpHeaders().set('Authorization',`Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJVc2VyIE1hbmFnZW1lbnQgUG9ydGFsIiwic3ViIjoiZmFyd2ExIiwiaXNzIjoiR2V0IEFycmF5cywgTExDIiwiZXhwIjoxNjUyNDMyNDE5LCJpYXQiOjE2NTIwMDA0MTksImF1dGhvcml0aWVzIjpbInVzZXI6cmVhZCIsInVzZXI6Y3JlYXRlIiwidXNlcjp1cGRhdGUiLCJ1c2VyOmRlbGV0ZSJdfQ.1cBZ-El79IjkoUTSbye8idcd1e4Rb_F6fg_PzBb84eQ8JAwvkbXdLRaNT3UfrV3VkhVdpRaBxUZ8bwY3i8yc1A"}`)});
+    //return this.http.request(req, this.httpOptions);
   }
 }
